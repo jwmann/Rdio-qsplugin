f8a759d514882d3f1337b5f3ccd3956bf759ea0a
jwmann <me@jameswmann.com>
2013-05-10 19:57:36 -0400

    Add Rdio Previous Track command

--- RdioPlugin/RdioPluginSource.m
+++ RdioPlugin/RdioPluginSource.m
@@ -70,7 +70,7 @@
 	NSString *actionID = nil;
 	NSDictionary *actionDict = nil;
 	// create catalog objects using info specified in the plist (under QSCommands)
-	NSArray *controls = [NSArray arrayWithObjects:@"RdioPluginPlayPause", @"RdioPluginNextTrack", nil];
+	NSArray *controls = [NSArray arrayWithObjects:@"RdioPluginPlayPause", @"RdioPluginNextTrack", @"RdioPluginPreviousTrack", nil];
 	for (NSString *control in controls) {
 		command = [QSCommand commandWithIdentifier:control];
 		if (command) {
